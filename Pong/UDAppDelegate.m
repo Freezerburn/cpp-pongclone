b714515c3f180be50a9900ddf4995b1a51745bef
Vincent Kuyatt <vincentk@arcode.com>
2013-12-31 09:10:25 -0500

    Initial Commit

---
+++ Pong/UDAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  UDAppDelegate.m
+//  Pong
+//
+//  Created by Vincent K on 12/31/13.
+//  Copyright (c) 2013 Unlocked Doors. All rights reserved.
+//
+
+#import "UDAppDelegate.h"
+
+@implementation UDAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ Pong/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Pong
+//
+//  Created by Vincent K on 12/31/13.
+//  Copyright (c) 2013 Unlocked Doors. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
